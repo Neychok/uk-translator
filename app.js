02af1e3c0abfc3b6c89d94b250b0cf366022ae99
Neycho Kalaydzhiev <n.kalaidjiev@gmail.com>
2024-07-16 17:49:34 +0300

    Changed delimiter

--- app.js
+++ app.js
@@ -11,57 +11,64 @@ try {
 		console.log('Translating file: ' + fileName);
 		translateCSV(fileName);
 		console.log('Translation complete.');
-
 	} else {
 		console.log('File does not exist.');
 		process.exit();
 	}
-} catch(err) {
-	console.error(err)
+} catch (err) {
+	console.error(err);
 }
 
 function translateCSV(file) {
 	fs.createReadStream(path.resolve(__dirname, file))
-	.pipe(csv.parse({ headers: true }))
-	.pipe(csv.format({ headers: true }))
-	.transform((row, next) => {
+		.pipe(csv.parse({ headers: true, delimiter: ';' }))
+		.pipe(csv.format({ headers: true }))
+		.transform((row, next) => {
+			let translatedWords = row.word_from;
+			let translations = translateWords(
+				stripper.stripHtml(row.word_from).result
+			);
+			if (false !== translations) {
+				for (let property in translations) {
+					translations[property].forEach(function (translation) {
+						for (let word in translation) {
+							console.log(
+								word + ' => ' + translation[word].details
+							);
+							translatedWords = translatedWords.replaceAll(
+								word,
+								translation[word].details
+							);
+						}
+					});
+				}
+			}
 
-		let translatedWords = row.word_from;
-		let translations = translateWords(stripper.stripHtml(row.word_from).result);
-		if ( false !== translations ) {
-			for (let property in translations) {
-				translations[property].forEach(function(translation) {
-					for ( let word in translation ) {
-						console.log(word + ' => ' + translation[word].details);
-						translatedWords = translatedWords.replaceAll(word, translation[word].details);
-					}
-				});
-				
-			};
-		}
+			row.word_to = translatedWords;
 
-		row.word_to = translatedWords;
-
-		return next(null, row);
-	})
-	.pipe(fs.createWriteStream(path.resolve(__dirname, file.replace('.csv', '-translated.csv'))))
-	.on('end', () => process.exit());
-
+			return next(null, row);
+		})
+		.pipe(
+			fs.createWriteStream(
+				path.resolve(__dirname, file.replace('.csv', '-translated.csv'))
+			)
+		)
+		.on('end', () => process.exit());
 }
 
 function translateWords(words) {
-
 	var options = {
-	  spelling: true,
-	  american: true
+		spelling: true,
+		american: true,
 	};
 
 	let translations = translator.translate(words, options);
 
-	if (translations
-		&& Object.keys(translations).length > 0
-		&& Object.getPrototypeOf(translations) === Object.prototype) {
-
+	if (
+		translations &&
+		Object.keys(translations).length > 0 &&
+		Object.getPrototypeOf(translations) === Object.prototype
+	) {
 		return translations;
 	}
 
